b50fb2d718ef60bdc9b2444c2c56a67718165008
apple <ndu5@illinois.edu>
2018-02-06 20:25:13 -0600

    Initial project commit2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(Webscraper.urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        System.out.println(Webscraper.urlToString("https://www.bls.gov/tus/charts/chart9.txt"));
+        System.out.println(Webscraper.urlToString("http://tgftp.nws.noaa.gov/data/raw/fz/fzus53.klot.srf.lot.txt"));
     }
 }
